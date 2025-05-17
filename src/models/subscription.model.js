588a55566bfe086dfcf29be1d31d28e8a5d0fd70
Utkarsh Agarwal <127212603+utkarshagar2121@users.noreply.github.com>
2025-05-17 15:59:01 +0530

    subscriptionModel

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscription = new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    channel:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    
+},{timestamps:true});
+
+export const Subscription =mongoose.model("subscriptions",subscription)
